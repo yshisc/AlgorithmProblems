cab98014661d33f5e69ec07103b5f79f316c63e3
Xiangfei Dong <xiangfeid@gmail.com>
2018-03-12 23:09:07 -0700

    One more solution and throw exception on bad param

--- Leetcode/006.ZigZagConversion.java
+++ Leetcode/006.ZigZagConversion.java
@@ -7,12 +7,17 @@ import java.util.List;
  *
  * 6. ZigZag Conversion
  *
- * Keep a list for each row in zigzag.
- * Keep a pointer swing between 0 ~ numRows-1
- * Go through each character of the string, add each character to a row.
  */
 public class ZigZagConversion {
-    public String convert(String s, int numRows) {
+    /**
+     * Solution 1:
+     *
+     * Keep a list for each row in zigzag.
+     * Keep a pointer swing between 0 ~ numRows-1
+     * Go through each character of the string, add each character to a row.
+     *
+     */
+    public String convert1(String s, int numRows) {
         if (numRows == 1) { //No need for convert.
             return s;
         }
@@ -46,6 +51,42 @@ public class ZigZagConversion {
             }
         }
 
+        return sb.toString();
+    }
+    
+     /**
+     * Solution 2:
+     *
+     * Keep a list for each row in zigzag.
+     * Use two loops to swing down and up.
+     *
+     */
+    public String convert2(String s, int numRows) {
+        if (numRows < 1) {
+            throw new IllegalArgumentException("Number of rows should be larger than 0!");
+        }
+        
+        final List<List<Character>> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new ArrayList<>());
+        }
+        
+        int i = 0;
+        while (i < s.length()) {
+            for (int j = 0; i < s.length() && j < numRows; i++, j++) {
+                rows.get(j).add(s.charAt(i));
+            }
+            for (int j = numRows - 2; i < s.length() && j > 0; i++, j--) {
+                rows.get(j).add(s.charAt(i));
+            }
+        }
+        
+        final StringBuilder sb = new StringBuilder();
+        for (final List<Character> row : rows) {
+            for (final char c : row) {
+                sb.append(c);
+            }
+        }
         return sb.toString();
     }
 }
