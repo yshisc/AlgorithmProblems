13f53ea07a046f574a1f24b0cf75216310fd0cea
Xiangfei Dong <xiangfeid@gmail.com>
2018-09-27 09:41:48 +0800

    Easier to understand

--- Leetcode/035.SearchInsertPosition.java
+++ Leetcode/035.SearchInsertPosition.java
@@ -4,26 +4,32 @@ package Leetcode;
  *
  * 35.Search Insert Position
  *
- * find first position of >= target.
- * !!Must consider if the insert position exceeds the range of array.
+ * Use binary search to narrow range to 2, then compare target to the 2 values.
  */
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
         int start = 0, end = nums.length - 1;
-        while (start < end) {
+        while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > target) {
-                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
             } else {
-                start = mid + 1;
+                end = mid;
             }
         }
-
+        
         if (nums[start] >= target) {
             return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
         }
-        return nums.length;
     }
 }
