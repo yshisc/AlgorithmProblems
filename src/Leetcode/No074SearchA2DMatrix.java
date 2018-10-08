b2bdb87279773273deeca816b4b6308965971c50
Bennett Dong <xiangfeid@gmail.com>
2018-10-07 17:00:48 -0700

    Alternative solutions

--- src/Leetcode/No074SearchA2DMatrix.java
+++ src/Leetcode/No074SearchA2DMatrix.java
@@ -3,21 +3,51 @@ package Leetcode;
  * @author xiangfeidong
  *
  * 74.Search a 2D Matrix
- *
- * Search from top left. Each time if the value is not target, discard a row or a column.
  */
 public class No074SearchA2DMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    /**
+     * Search from top right. Each time if the value is not target, discard a row or a column.
+     */
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+
         int i = 0, j = matrix[0].length - 1;
         while (i < matrix.length && j >= 0) {
-            if (target == matrix[i][j]) {
+            if (matrix[i][j] == target) {
                 return true;
-            } else if (target < matrix[i][j]) {
-                j--;
-            } else {
+            } else if (matrix[i][j] < target) {
                 i++;
+            } else {
+                j--;
             }
         }
+
         return false;
+    }
+
+    /**
+     * Flatten 2D matrix and do binary search.
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+
+        int start = 0, end = matrix.length * matrix[0].length - 1;
+        while (start < end) {
+            final int mid = start + (end - start) / 2;
+            final int value = matrix[mid / matrix[0].length][mid % matrix[0].length];
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return matrix[start / matrix[0].length][start % matrix[0].length] == target;
     }
 }
