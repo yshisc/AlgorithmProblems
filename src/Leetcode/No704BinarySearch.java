f6bcb5f643509415a39204505875be3f0acf638d
Bennett Dong <xiangfeid@gmail.com>
2018-10-09 21:48:27 -0700

    Binary Search

---
+++ src/Leetcode/No704BinarySearch.java
@@ -0,0 +1,30 @@
+package Leetcode;
+
+/**
+ * 74. Binary Search
+ */
+public class No704BinarySearch {
+    public int search(final int[] nums, final int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            final int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        } else {
+            return -1;
+        }
+    }
+}
