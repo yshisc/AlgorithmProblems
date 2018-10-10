39421395a86086476ab1e3781c8a95fa397ce23c
Bennett Dong <xiangfeid@gmail.com>
2018-10-09 22:46:21 -0700

    nit

--- src/Leetcode/No050Pow.java
+++ src/Leetcode/No050Pow.java
@@ -11,12 +11,12 @@ package Leetcode;
  *            if n is odd, return pow(x,n/2) * pow(x,n/2)/x
  */
 public class No050Pow {
-    public double myPow(double x, int n) {
+    public double myPow(final double x, final int n) {
         if (n == 0) {
             return 1;
         }
 
-        double temp = myPow(x, n / 2);
+        final double temp = myPow(x, n / 2);
         if (n % 2 == 0) {
             return temp * temp;
         } else if (n > 0) {
